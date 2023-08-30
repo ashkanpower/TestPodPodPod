21b46c6c6a496c065b886175ab6555adee0101b7
ashkan <ashkanpower@gmail.com>
2023-08-30 17:55:24 +0330

    addedClass

---
+++ Classes/Student.swift
@@ -0,0 +1,17 @@
+//
+//  Student.swift
+//  Pods-TestPodPodPod_Example
+//
+//  Created by ashkan ghodrat on 6/8/1402 AP.
+//
+
+import Foundation
+
+class Student {
+    
+    var name = "Ashkan"
+    
+    public func printName(){
+        print(name)
+    }
+}
